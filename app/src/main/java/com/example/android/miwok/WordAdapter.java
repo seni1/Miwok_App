59aeab72253c48c798d90a601ddb890bb93beb14
seni1 <seni@bestpick.ru>
2018-09-23 17:23:46 +0300

    Partial application of RecycleView 2

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -2,17 +2,11 @@ package com.example.android.miwok;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 /**
  * {@link WordAdapter} is an {@link android.widget.ArrayAdapter} that can provide the layout for each list
@@ -31,6 +25,8 @@ import java.util.ArrayList;
 
 public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordAdapterViewholder>{
 
+    private String mWordData;
+
     /** Resource ID for the background color for this list of words */
     private int mColorResourceID;
 
@@ -42,16 +38,37 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordAdapterVie
         mColorResourceID = colorResourceID;
     }
 
+    public class WordAdapterViewholder extends RecyclerView.ViewHolder {
+
+        public final TextView mWordTextView;
+
+        public WordAdapterViewholder(View view) {
+
+            super(view);
+
+            mWordTextView = view.findViewById(R.id.text_container);
+
+        }
+    }
 
 
     @NonNull
     @Override
-    public WordAdapter.WordAdapterViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public WordAdapterViewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new WordAdapterViewholder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull WordAdapter.WordAdapterViewholder holder, int position) {
+    public void onBindViewHolder(@NonNull WordAdapterViewholder holder, int position) {
+
+        String w
 
     }
 
@@ -60,11 +77,7 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordAdapterVie
         return 0;
     }
 
-    public class WordAdapterViewholder extends RecyclerView.ViewHolder {
-        public WordAdapterViewholder(View itemView) {
-            super(itemView);
-        }
-    }
+
 
 
 //    /**
